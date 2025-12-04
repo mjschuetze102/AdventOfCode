3377a6390df3805c43f22ada4c5313c13d913ee9
Michael Schuetze <mjschuetze102@gmail.com>
2025-12-03 23:27:27 -0600

    Completed Day 4

---
+++ src/2025/Day04/Part2.py
@@ -0,0 +1,47 @@
+import itertools
+
+
+grid = []
+with open("/home/user/Downloads/advent.txt") as file:
+    for line in file:
+        grid.append(list(line.strip()))
+
+
+def number_of_matches(grid, position):
+    directions_to_check = [[[-1, -1], [-1, 0], [-1, 1]],  # noqa: E201
+                           [[ 0, -1],          [ 0, 1]],  # noqa: E201
+                           [[ 1, -1], [ 1, 0], [ 1, 1]]]  # noqa: E201
+
+    height, width = len(grid), len(grid[0])
+    if position[0] <= 0:
+        del directions_to_check[0]
+
+    if position[0] >= height - 1:
+        del directions_to_check[-1]
+
+    if position[1] <= 0:
+        directions_to_check = [group[1:] for group in directions_to_check]
+
+    if position[1] >= width - 1:
+        directions_to_check = [group[:-1] for group in directions_to_check]
+
+    directions_to_check = list(itertools.chain.from_iterable(directions_to_check))
+
+    matches = 0
+    for [row, col] in directions_to_check:
+        matches += grid[position[0] + row][position[1] + col] == "@"
+
+    return matches
+
+
+num, prevNum = 0, -1
+while prevNum != num:
+    prevNum = num
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == "@":
+                if number_of_matches(grid, [row, col]) < 4:
+                    grid[row][col] = "."
+                    num += 1
+
+print(num)
